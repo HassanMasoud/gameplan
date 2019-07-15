1ab5692486d2cab415bac77b0442f8d921a91b05
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2019-07-15 14:34:44 +0300

    adding dummy data

--- gameplan/src/components/projects/ProjectList.js
+++ gameplan/src/components/projects/ProjectList.js
@@ -2,17 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import ProjectSummary from "./ProjectSummary";
 
-const ProjectList = props => {
+const ProjectList = ({ projects }) => {
   return (
     <div className="project-list section">
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
+      {projects &&
+        projects.map(project => {
+          return <ProjectSummary project={project} key={project.id} />;
+        })}
     </div>
   );
 };
 
-ProjectList.propTypes = {};
+ProjectList.propTypes = {
+  projects: PropTypes.array.isRequired
+};
 
 export default ProjectList;
--- gameplan/src/components/projects/ProjectSummary.js
+++ gameplan/src/components/projects/ProjectSummary.js
@@ -1,11 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const ProjectSummary = props => {
+const ProjectSummary = ({ project }) => {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text-darken-3">
-        <span className="card-title">Project Title</span>
+        <span className="card-title">{project.title}</span>
         <p>Posted by Hassan Masoud</p>
         <p className="grey-text">3rd September, 2am</p>
       </div>
@@ -13,6 +13,8 @@ const ProjectSummary = props => {
   );
 };
 
-ProjectSummary.propTypes = {};
+ProjectSummary.propTypes = {
+  project: PropTypes.object.isRequired
+};
 
 export default ProjectSummary;
