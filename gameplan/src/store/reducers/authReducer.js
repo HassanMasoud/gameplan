f09e8e7a0bfc87bd061e99ef25001e02a8366a5e
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2019-07-16 12:44:06 +0300

    firebase login functionality

--- gameplan/src/store/reducers/authReducer.js
+++ gameplan/src/store/reducers/authReducer.js
@@ -1,7 +1,25 @@
-const initialState = {};
+const initialState = {
+  authError: null
+};
 
 const authReducer = (state = initialState, action) => {
-  return state;
+  const { type } = action;
+  switch (type) {
+    case "LOGIN_ERROR":
+      console.log("login error");
+      return {
+        ...state,
+        authError: "Login failed"
+      };
+    case "LOGIN_SUCCESS":
+      console.log("login success");
+      return {
+        ...state,
+        authError: null
+      };
+    default:
+      return state;
+  }
 };
 
 export default authReducer;
