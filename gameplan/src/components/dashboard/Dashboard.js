62a47f577a122f462a2070167925b8e1cc416c96
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2019-07-17 10:27:50 +0300

    style changes

--- gameplan/src/components/dashboard/Dashboard.js
+++ gameplan/src/components/dashboard/Dashboard.js
@@ -12,12 +12,12 @@ class Dashboard extends Component {
     if (!auth.uid) return <Redirect to="/signin" />;
 
     return (
-      <div className="dashborad container">
-        <div className="row">
-          <div className="col s12 m6">
+      <div className="dashboard container">
+        <div className="row home">
+          <div className="col s12 m6" id="landing">
             <ProjectList projects={projects} />
           </div>
-          <div className="col s12 m5 offset-m1">
+          <div className="col s12 m5 offset-m1" id="notifications">
             <Notifications notifications={notifications} />
           </div>
         </div>
@@ -38,7 +38,7 @@ const mapStateToProps = state => {
 export default compose(
   connect(mapStateToProps),
   firestoreConnect([
-    { collection: "projects" },
-    { collection: "notifications", limit: 3 }
+    { collection: "projects", orderBy: ["createdAt", "desc"] },
+    { collection: "notifications", limit: 3, orderBy: ["time", "desc"] }
   ])
 )(Dashboard);
