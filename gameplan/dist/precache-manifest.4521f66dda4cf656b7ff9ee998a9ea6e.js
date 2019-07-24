021abd1d834c711840632330d8e6df6f63fe738c
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2019-07-24 11:11:50 +0300

    prepared for deployment

---
+++ gameplan/dist/precache-manifest.4521f66dda4cf656b7ff9ee998a9ea6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c861bd15185abe9455313a3e1e2526",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5d47da89048f4714f62",
+    "url": "/static/css/main.5ce0e4c6.chunk.css"
+  },
+  {
+    "revision": "4f5050f646e402046754",
+    "url": "/static/js/2.78c87efc.chunk.js"
+  },
+  {
+    "revision": "a5d47da89048f4714f62",
+    "url": "/static/js/main.8e9c0687.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
